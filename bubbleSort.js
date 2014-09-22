6ce158ae9de443066a08c530fd7d22b3c5d27e45
Roman Zhak <zhak55@ya.ru>
2014-09-22 23:03:08 +0400

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,7 +1,7 @@
 function bubbleSort( arr, inv ) {
  var j , i = 0, $length = arr.length, $interim;
    for( ; i < $length - 1; i++ ) 
-     for( j = 0; j < $length  - i - 1; j++ ) if( arr[j] > arr[j + 1])
+     for( j = 0; j < $length  - i - 1; j++ ) if( arr[j] > arr[j + 1] )
        {
          $interim = arr[j];
          arr[j] = arr[j + 1];
