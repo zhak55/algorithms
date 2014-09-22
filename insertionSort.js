5527ed495347d5e0489a56cf9d399ed47048bf81
Roman Zhak <zhak55@ya.ru>
2014-09-22 17:51:58 +0400

    Create insertionSort.js

---
+++ insertionSort.js
@@ -0,0 +1,17 @@
+// Should remember:
+// This algorithm works efficiently for small data sets or 
+// if your data set has already been partially sorted 
+
+function InsertionSort( arr ) {
+ var i = 0, $interim, $prev;
+    for( ; i < arr.length; i ++ ) {
+      $interim = arr[i];
+      $prev = i - 1;
+        while( $prev >=0 && arr[$prev] > $interim ) {
+           arr[$prev + 1] = arr[$prev];
+           arr[$prev] = $interim;
+           $prev--;
+        }
+    }
+  return arr;
+};
