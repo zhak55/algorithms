2b26cb9916dcce4b5fea65dd5220b0d9dc635c10
Roman Zhak <zhak55@ya.ru>
2014-09-22 21:48:40 +0400

    Create heapSort.js

---
+++ heapSort.js
@@ -0,0 +1,39 @@
+function Heapsort( arr ) {
+  this.$arr = arr;
+  this.heapSize  = arr.length;
+  this.createHeap();
+    while( this.heapSize > 1 ) {
+      this.swap( 0, this.heapSize - 1 );
+      this.heapSize--;
+      this.heapify( 0 );
+    }
+  return this.$arr;
+};
+
+Heapsort.prototype.createHeap = function() {
+   var heapSize = this.$arr.length;
+   var i = Math.round( heapSize / 2 );
+     for ( ; i >= 0; i-- ) this.heapify( i );   
+};
+
+Heapsort.prototype.heapify = function( i ) {
+  var left  = this.swipe( i , true )
+  ,   right = this.swipe( i ) 
+  ,   largest = i;
+   if( left  < this.heapSize && this.$arr[i] < this.$arr[left] ) largest = left ;
+   if( right < this.heapSize && this.$arr[largest] < this.$arr[right] ) largest = right;
+   if( i !== largest ) {
+        this.swap( i , largest )
+        this.heapify( largest ) 
+    }
+};
+
+Heapsort.prototype.swipe = function( i , l ) {
+    return l ?  ( 2 * i + 2 ) :  ( 2 * i + 1 );  
+};
+
+Heapsort.prototype.swap = function( i , j ) {
+  var $interim = this.$arr[i];
+       this.$arr[i] = this.$arr[j];
+       this.$arr[j] = $interim;
+};
